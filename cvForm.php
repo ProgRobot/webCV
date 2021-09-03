6737566ee4148a53dc744b68a45d2456b32d970e
YoucefKF <kfyoucef@gmail.com>
2021-09-03 18:00:36 +0200

    Preparation backend

--- cvForm.php
+++ cvForm.php
@@ -8,7 +8,11 @@
     <title>Add project</title>
   </head>
   <body>
-    <!-- //TODO Création du formulaire et de la base de donnée -->
+
+  <?php
+      //TODO Création du formulaire et de la base de donnée 
+   ?>
+    
 
     <form action="cvForm.php" method="post">
       <fieldset>
@@ -17,16 +21,25 @@
       <input id="projectTitleField" name="projectTitle" type="text" size="128" placeholder="Exemple: Réalisation d'un drone quadrirotor"><br><br>
       <label for="dateDebutField"><b>Commencé le:</b></label>
       <input type="date" name="DateDebut" id="dateDebutField">
-      <label for="dateDebutField"><b>Fini le:</b></label>
+      <label id="finiLeLabel" for="dateFinField"><b>Fini le:</b></label>
       <input type="date" name="DateFin" id="dateFinField"><br><br>
       <label for="projectDescription"><b>Description:</b></label><br>
       <textarea name="projectDescriptionField" id="projectDescription" placeholder="Exemple: Ce projet consiste à la réalisation d'un drone quadrirotor, le but est de réussir à stabiliser le drone dans une altitude dans des conditions climatiques normal..." cols="130" rows="10"></textarea><br><br>
-       <label for="usedLogicField"><b>Logique utilisée:</b></label><br>
-       <textarea name="projectLogic" id="usedLogicField" placeholder="Exemple: J'ai commencé à réaliser le modele mathématique du drone sous Matlab Simulink, j'ai simulé en ajustant les paramètres du système et de la commande pour garantir les spécifications du cahier des charges...etc " cols="130" rows="10"></textarea><br><br>
+      <label for="usedLogicField"><b>Logique utilisée:</b></label><br>
+      <textarea name="projectLogic" id="usedLogicField" placeholder="Exemple: J'ai commencé à réaliser le modele mathématique du drone sous Matlab Simulink, j'ai simulé en ajustant les paramètres du système et de la commande pour garantir les spécifications du cahier des charges...etc " cols="130" rows="10"></textarea><br><br>
       <label for="projectSkillsField"><b>Compétences acquises:</b></label><br>
       <input id="projectSkillsField" name="projectSkills" type="text" size="144" placeholder="Exemple: Solidworks, Matlab/Simulink, Modelisation ... etc "><br><br>
-    </fieldset>
-    </form>
+      <input id="btnAjouterProjetForm" type="submit" name="btnAjouter" value="Ajouter+">
+      <?php 
+      
+    if (isset($_POST['projectTitle']) && isset($_POST['DateDebut']) && isset($_POST['DateFin'])&&isset($_POST['projectDescriptionField'])&&isset($_POST['projectLogic'])&&isset($_POST['projectSkills'])&&isset($_POST['btnAjouter'])) {
+      echo "Reccupération réussi";
+      //TODO connexion à la BD
+    }
+
+    echo "</fieldset></form>";
+    ?>
+
 
   </body>
 </html>
